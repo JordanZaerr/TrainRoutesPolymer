fbdfdc4a1bb5bb402669b46ba3224b3ff8e11f8e
JordanZaerr <jordan.zaerr@extendhealth.com>
2014-09-08 08:00:40 -0600

    Initial code commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-TrainRoutesPolymer
-==================
+Train Routes with Polymer
+=========================
+
+This application uses Polymer to display where the Blue Line, GreenLine, Red line, and S-Line Street Car trains are for UTA in Utah.
+
+Prerequisites
+-------------
+- [NodeJS][Node] installed 
+[Node]:http://nodejs.org/
+[nInspect]:https://github.com/node-inspector/node-inspector
+
+Steps to get running
+--------------------
+- Clone repository
+- Run ```npm install``` to install the dependencies (run from folder where you cloned the repo)
+- Run ```npm start``` to start up the server.
+- Navigate to http:\\\\localhost:3000 in your browser
+
+Steps to debug
+--------------
+- Have node inspector installed ```npm install -g node-inspector```
+- Run ```node-debug server.js``` This will launch the debugger site in your browser. This only works in a webkit browser, chrome or safari. If you default        browser isn't one of these then copy the url into one of those browsers.
+- Navigate to http:\\\\localhost:3000 in your browser.
---
+++ server.js
@@ -0,0 +1,66 @@
+var express = require("express"),
+	http = require("http"),
+	path = require("path"),
+	request = require("request"),
+	parser = require("xml2js"),
+	Q = require("q"),
+	imageMap = {"701":"img/blue.png", "703":"img/red.png", "704":"img/green.png", "720":"img/purple.png"},
+	app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/route', function(req,res){
+	var routeNum = req.query.number || "703";
+	var routesToGrab = [];
+
+	if(routeNum == '-1'){
+		routesToGrab = [
+		getTrainData("701"),
+		getTrainData("703"),
+		getTrainData("704"),
+		getTrainData("720")
+		];
+	}
+	else{
+		routesToGrab.push(getTrainData(routeNum));
+	}
+
+	Q.all(routesToGrab)
+	.then(function(values){
+		var flattened = values.reduce(function(p,c){ return p.concat(c); }, []);
+		res.send(JSON.stringify(flattened));
+	})
+	.catch(function(error){
+		console.log('Error with getting route data: ' + error);
+	});
+});
+
+function getTrainData(routeNum){
+	var deferred = Q.defer();
+	var result = request("http://api.rideuta.com/SIRI/SIRI.svc/VehicleMonitor/ByRoute?route=" + routeNum + "&usertoken=UOJCQBT0TIZ",
+		function(error, response, body){
+			var trainData = [];	
+			parser.parseString(body, function(err, result){
+				if(err || !result.Siri){
+					deferred.reject(err || 'Invalid response');
+					return;
+				}
+				var trains = result.Siri.VehicleMonitoringDelivery[0].VehicleActivity[0].MonitoredVehicleJourney;
+				trains.forEach(function(train){
+					trainData.push({
+						"direction" : train.DirectionRef[0].toLowerCase(),
+						"vehicleId" : train.VehicleRef[0],
+						"longitude" : train.VehicleLocation[0].Longitude[0],
+						"latitude" : train.VehicleLocation[0].Latitude[0],
+						"icon" : imageMap[routeNum] || 'img/black.png'
+					});
+				});
+			});
+			deferred.resolve(trainData);
+		});
+	return deferred.promise;
+};
+
+var server = app. listen(3000, function(){
+	console.log('Listening on port %d', server.address().port);
+});
